94e9c6171bfdf8c3f0d4974cf550d5aaa221e343
iremB <irem.belik@student.reutlingen.university.de>
2021-06-29 17:55:02 +0200

    Testdaten

---
+++ sportgeraete.sql
@@ -0,0 +1,61 @@
+insert into TRAINER values('SG_001','Aerobic-Stepper', 'Reebok', '742735785-X',
+insert into TRAINER values('SG_002','Bauchtrainer', 'Sport-Thieme', '663053155-8',
+insert into TRAINER values('SG_003','Boxsack', 'Super Pro', '210999168-2',
+insert into TRAINER values('SG_004','Crosstrainer', 'Sport-Thieme', '374455454-6')
+insert into TRAINER values('SG_005','Hantelbank', 'Sport-Thieme', '016260692-3')
+insert into TRAINER values('SG_006','Hantelscheiben','Sport-Thieme', '314255292-8')
+insert into TRAINER values('SG_007','Hantelstange', 'Balanced-Body')
+insert into TRAINER values('SG_008','Kettlebells', 'Balanced-Body')
+insert into TRAINER values('SG_009','Klimmzugstangen', 'Sport-Thieme')
+insert into TRAINER values('SG_010','Indoor Bikes', 'Balanced-Body')
+insert into TRAINER values('SG_011','Pilates', 'Balanced-Body')
+insert into TRAINER values('SG_012','Kraftgeraet', 'Life Fitness')
+insert into TRAINER values('SG_013','Laufband', 'Stages')
+insert into TRAINER values('SG_014','Gymnastikball', 'Reebok')
+insert into TRAINER values('SG_015','Fitnessmatte', 'Reebok')
+insert into TRAINER values('SG_016','Battle Rope', 'Stages')
+insert into TRAINER values('SG_017','Crosstrainer', 'Balanced-Body')
+insert into TRAINER values('SG_018','Schrägbank', 'Stages')
+insert into TRAINER values('SG_019','Aerobic-Stepper', 'Reebok')
+insert into TRAINER values('SG_020','Beinpresse', 'Balanced-Body')
+insert into TRAINER values('SG_021','Bauchtrainer', 'Stages')
+insert into TRAINER values('SG_022','Boxsack', 'Balanced-Body')
+insert into TRAINER values('SG_023','Crosstrainer', 'Life Fitness')
+insert into TRAINER values('SG_024','Hantelbank', 'Stages')
+insert into TRAINER values('SG_025','Hantelscheiben', 'Life Fitness')
+insert into TRAINER values('SG_026','Hantelstange', 'Balanced-Body')
+insert into TRAINER values('SG_027','Beinpresse', 'Stages')
+insert into TRAINER values('SG_028','Klimmzugstangen', 'Life Fitness')
+insert into TRAINER values('SG_029','Indoor Bikes', 'Life Fitness')
+insert into TRAINER values('SG_030','Pilates', 'Balanced-Body')
+
+('742735785-X', '2/8/2021', '3/3/2021', '8/15/2020');
+('663053155-8', '5/17/2021', '5/29/2021', '4/4/2021');
+('210999168-2', '9/20/2020', '3/23/2021', '2/18/2021');
+('374455454-6', '12/13/2020', '6/13/2021', '12/21/2020');
+('016260692-3', '4/29/2021', '9/23/2020', '10/28/2020');
+('314255292-8', '3/8/2021', '7/23/2020', '2/21/2021');
+('552930938-0', '9/18/2020', '11/15/2020', '5/2/2021');
+('442233095-0', '11/21/2020', '2/12/2021', '1/3/2021');
+('853798453-1', '8/5/2020', '2/3/2021', '1/6/2021');
+('471034025-0', '2/8/2021', '9/2/2020', '11/2/2020');
+('812701031-6', '2/4/2021', '10/25/2020', '4/8/2021');
+('746206818-4', '11/14/2020', '6/11/2021', '3/1/2021');
+('859856964-X', '3/24/2021', '2/7/2021', '12/14/2020');
+('523936375-7', '9/18/2020', '1/11/2021', '10/9/2020');
+('811304171-0', '8/22/2020', '5/14/2021', '9/2/2020');
+('932074453-X', '6/24/2021', '10/7/2020', '1/20/2021');
+('396263007-4', '3/17/2021', '5/18/2021', '9/18/2020');
+('615180930-0', '2/9/2021', '9/2/2020', '4/29/2021');
+('001506621-5', '11/21/2020', '1/14/2021', '12/25/2020');
+('758115842-X', '7/11/2020', '2/5/2021', '11/8/2020');
+('825349446-7', '10/26/2020', '10/3/2020', '4/30/2021');
+('542151925-2', '3/25/2021', '8/16/2020', '9/1/2020');
+('078817561-0', '6/14/2021', '1/22/2021', '5/5/2021');
+('390461963-2', '1/26/2021', '11/21/2020', '11/13/2020');
+('699843581-9', '11/26/2020', '11/25/2020', '11/16/2020');
+('199628774-5', '5/4/2021', '5/27/2021', '2/12/2021');
+('078809144-1', '3/3/2021', '4/13/2021', '12/9/2020');
+('721500420-1', '3/14/2021', '9/6/2020', '1/15/2021');
+('836415635-7', '4/4/2021', '6/20/2021', '7/25/2020');
+('982506283-5', '8/25/2020', '6/20/2021', '10/31/2020');
