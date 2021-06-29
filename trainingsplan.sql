f0ef55660ef2e95b24fd109e2b2fa6bbbca7d2ec
Okan Kaya <kaya.okan@live.de>
2021-06-29 23:00:54 +0200

    indiplan rein gemacht

--- trainingsplan.sql
+++ trainingsplan.sql
@@ -1,5 +1,5 @@
-insert into TRAININGSPLAENE values (TP_01, 'Beginner Friendly', 1, 6);
-insert into TRAININGSPLAENE values (TP_02, 'Ganzkörper', 2, 12);
-insert into TRAININGSPLAENE values (TP_03, 'Muskelaufbau', 4, 8);
-insert into TRAININGSPLAENE values (TP_04, '12-Wochen Challenge', 5, 12);
-insert into TRAININGSPLAENE values (TP_05, 'Zirkeltraining', 5, 12);
+insert into TRAININGSPLAENE values ('TP_01', 'Beginner Friendly', 1, 6);
+insert into TRAININGSPLAENE values ('TP_02', 'Ganzkörper', 2, 12);
+insert into TRAININGSPLAENE values ('TP_03', 'Muskelaufbau', 4, 8);
+insert into TRAININGSPLAENE values ('TP_04', '12-Wochen Challenge', 5, 12);
+insert into TRAININGSPLAENE values ('TP_05', 'Zirkeltraining', 5, 12);
