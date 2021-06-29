2766ad074f180adbb49d17d99794db8c23ad0973
iremB <irem.belik@student.reutlingen.university.de>
2021-06-29 15:22:06 +0200

    Neue Skripte

---
+++ trainer.sql
@@ -0,0 +1,6 @@
+insert into TRAINER values ('A','M_05');
+insert into TRAINER values ('C','M_06');
+insert into TRAINER values ('B','M_08');
+insert into TRAINER values ('C','M_13');
+insert into TRAINER values ('B','M_14');
+commit;
