c81cb29d41e91559334edf79081582dad9ab1395
Till-G <t.greiffertmail.com>
2021-06-30 01:24:31 +0200

    fast geschafft

--- data.sql
+++ data.sql
@@ -593,55 +593,56 @@ INSERT INTO steht_unter_vertrag VALUES ('M_09','AV_08');
 ------------------------------------------------------------------------------------------------------------------------
 
 -- TODO: Vorgaenger, Nachfolger, mehrfacheintraege(?)
+-- TODO: Hier koennte es ganz bitter werden: Referenz auf einen Nachfolger, der noch nicht erstellt ist, geht leider nicht :'(
 
-INSERT INTO trainingseinheiten VALUES ('TE_00', 'Stepper', 20, 'Stepper Cardio Training', null, 'TE_12', 'SG_001');
-INSERT INTO trainingseinheiten VALUES ('TE_01', 'Stepper', 20, 'Stepper Cardio Training', null, 'TE_16', 'SG_002');
-INSERT INTO trainingseinheiten VALUES ('TE_02', 'Stepper', 20, 'Stepper Cardio Training', null, 'TE_20', 'SG_003');
-INSERT INTO trainingseinheiten VALUES ('TE_03', 'Laufband', 20, 'Laufband Cardio Training', null, 'TE_13', 'SG_004');
-INSERT INTO trainingseinheiten VALUES ('TE_04', 'Laufband', 20, 'Laufband Cardio Training', null, 'TE_17', 'SG_005');
-INSERT INTO trainingseinheiten VALUES ('TE_05', 'Laufband', 20, 'Laufband Cardio Training', null, 'TE_21', 'SG_006');
-INSERT INTO trainingseinheiten VALUES ('TE_06', 'Crosstrainer', 30, 'Crosstrainer Workout', null, 'TE_14', 'SG_007');
-INSERT INTO trainingseinheiten VALUES ('TE_07', 'Crosstrainer', 30, 'Crosstrainer Workout', null, 'TE_18', 'SG_008');
-INSERT INTO trainingseinheiten VALUES ('TE_08', 'Crosstrainer', 30, 'Crosstrainer Workout', null, 'TE_22', 'SG_009');
-INSERT INTO trainingseinheiten VALUES ('TE_09', 'Biking', 30, 'Fahrrad fahren', null, 'TE_15', 'SG_010');
-INSERT INTO trainingseinheiten VALUES ('TE_10', 'Biking', 30, 'Fahrrad fahren', null, 'TE_19', 'SG_011');
-INSERT INTO trainingseinheiten VALUES ('TE_11', 'Biking', 30, 'Fahrrad fahren', null, 'TE_23', 'SG_012');
-INSERT INTO trainingseinheiten VALUES ('TE_12', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_00', null, 'SG_013');
-INSERT INTO trainingseinheiten VALUES ('TE_13', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_03', null, 'SG_013');
-INSERT INTO trainingseinheiten VALUES ('TE_14', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_06', null, 'SG_014');
-INSERT INTO trainingseinheiten VALUES ('TE_15', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_09', null, 'SG_014');
-INSERT INTO trainingseinheiten VALUES ('TE_13', 'Schraekbankdruecken', 12, 'Hanteltraining mit x Gewicht', 'te_57', 'te_37', 'sg_786');
-INSERT INTO trainingseinheiten VALUES ('TE_14', 'Schraekbankdruecken', 12, 'Hanteltraining mit x Gewicht', 'te_25', 'te_44', 'sg_167');
-INSERT INTO trainingseinheiten VALUES ('TE_15', 'SZ-Bizeps-Curls', 10, 'Hanteltraining mit x Gewicht', 'te_07', 'te_87', 'sg_196');
-INSERT INTO trainingseinheiten VALUES ('TE_16', 'Butterfly', 10, 'Training an Maschine mit x Gewicht', 'te_01', 'te_33', 'sg_266');
-INSERT INTO trainingseinheiten VALUES ('TE_17', 'Reverse-Butterfly', 10, 'Training an Maschine mit x Gewicht', 'te_88', 'te_57', 'sg_874');
-INSERT INTO trainingseinheiten VALUES ('TE_18', 'Brustpresse', 15, 'Training an Maschine mit x Gewicht', 'te_72', 'te_95', 'sg_681');
-INSERT INTO trainingseinheiten VALUES ('TE_19', 'Beipresse', 12, 'Training an Maschine mit x Gewicht', 'te_77', 'te_08', 'sg_898');
-INSERT INTO trainingseinheiten VALUES ('TE_20', 'Bauchpresse', 10, 'Training an Maschine mit x Gewicht', 'te_66', 'te_75', 'sg_700');
-INSERT INTO trainingseinheiten VALUES ('TE_21', 'Rückenstrecker',10, 'Training an Maschine mit x Gewicht', 'te_35', 'te_98', 'sg_062');
-INSERT INTO trainingseinheiten VALUES ('TE_22', 'Beinstrecker',10, 'Training an Maschine mit x Gewicht', 'te_24', 'te_27', 'sg_071');
-INSERT INTO trainingseinheiten VALUES ('TE_23', 'Beinbeuger', 10, 'Training an Maschine mit x Gewicht', 'te_97', 'te_44', 'sg_385');
-INSERT INTO trainingseinheiten VALUES ('TE_24', 'Kniebeuge', 15, 'Hanteltraining mit x Gewicht', 'te_79', 'te_24', 'sg_129');
-INSERT INTO trainingseinheiten VALUES ('TE_25', 'Kreuzheben',20, 'Hanteltraining mit x Gewicht', 'te_02', 'te_20', 'sg_664');
-INSERT INTO trainingseinheiten VALUES ('TE_26', 'Schulterpresse',15, 'Hanteltraining mit x Gewicht', 'te_38', 'te_14', 'sg_448');
-INSERT INTO trainingseinheiten VALUES ('TE_27', 'Bein Abduktion',10, 'Training an Maschine mit x Gewicht', 'te_59', 'te_38', 'sg_032');
-INSERT INTO trainingseinheiten VALUES ('TE_28', 'Bein Adduktion',10, 'Training an Maschine mit x Gewicht', 'te_26', 'te_74', 'sg_001');
-INSERT INTO trainingseinheiten VALUES ('TE_29', 'Latisimuszug',12, 'Training am Kabelturm mit x Gewicht', 'te_99', 'te_94', 'sg_247');
-INSERT INTO trainingseinheiten VALUES ('TE_30', 'Rudern',12, 'Training am Kabelturm mit x Gewicht', 'te_35', 'te_97', 'sg_708');
-INSERT INTO trainingseinheiten VALUES ('TE_31', 'Trizeps Extension am Kabelturm',8, 'Training am Kabelturm mit x Gewicht', 'te_23', 'te_66', 'sg_152');
-INSERT INTO trainingseinheiten VALUES ('TE_32', 'Bizeps Curls am Kabelturm',8, 'Training am Kabelturm mit x Gewicht', 'te_75', 'te_41', 'sg_608');
-INSERT INTO trainingseinheiten VALUES ('TE_33', 'Klimmzüge',10, 'Körpergewichtsübung', 'te_61', 'te_96', 'sg_824');
-INSERT INTO trainingseinheiten VALUES ('TE_34', 'Dips',10, 'Körpergewichtsübung', 'te_61', 'te_96', 'sg_824');
+-- INSERT INTO trainingseinheiten VALUES ('TE_00', 'Stepper', 20, 'Stepper Cardio Training', null, 'TE_12', 'SG_001');
+-- INSERT INTO trainingseinheiten VALUES ('TE_01', 'Stepper', 20, 'Stepper Cardio Training', null, 'TE_16', 'SG_002');
+-- INSERT INTO trainingseinheiten VALUES ('TE_02', 'Stepper', 20, 'Stepper Cardio Training', null, 'TE_20', 'SG_003');
+-- INSERT INTO trainingseinheiten VALUES ('TE_03', 'Laufband', 20, 'Laufband Cardio Training', null, 'TE_13', 'SG_004');
+-- INSERT INTO trainingseinheiten VALUES ('TE_04', 'Laufband', 20, 'Laufband Cardio Training', null, 'TE_17', 'SG_005');
+-- INSERT INTO trainingseinheiten VALUES ('TE_05', 'Laufband', 20, 'Laufband Cardio Training', null, 'TE_21', 'SG_006');
+-- INSERT INTO trainingseinheiten VALUES ('TE_06', 'Crosstrainer', 30, 'Crosstrainer Workout', null, 'TE_14', 'SG_007');
+-- INSERT INTO trainingseinheiten VALUES ('TE_07', 'Crosstrainer', 30, 'Crosstrainer Workout', null, 'TE_18', 'SG_008');
+-- INSERT INTO trainingseinheiten VALUES ('TE_08', 'Crosstrainer', 30, 'Crosstrainer Workout', null, 'TE_22', 'SG_009');
+-- INSERT INTO trainingseinheiten VALUES ('TE_09', 'Biking', 30, 'Fahrrad fahren', null, 'TE_15', 'SG_010');
+-- INSERT INTO trainingseinheiten VALUES ('TE_10', 'Biking', 30, 'Fahrrad fahren', null, 'TE_19', 'SG_011');
+-- INSERT INTO trainingseinheiten VALUES ('TE_11', 'Biking', 30, 'Fahrrad fahren', null, 'TE_23', 'SG_012');
+-- INSERT INTO trainingseinheiten VALUES ('TE_12', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_00', null, 'SG_013');
+-- INSERT INTO trainingseinheiten VALUES ('TE_13', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_03', null, 'SG_013');
+-- INSERT INTO trainingseinheiten VALUES ('TE_14', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_06', null, 'SG_014');
+-- INSERT INTO trainingseinheiten VALUES ('TE_15', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_09', null, 'SG_014');
+-- INSERT INTO trainingseinheiten VALUES ('TE_13', 'Schraekbankdruecken', 12, 'Hanteltraining mit x Gewicht', 'te_57', 'te_37', 'sg_786');
+-- INSERT INTO trainingseinheiten VALUES ('TE_14', 'Schraekbankdruecken', 12, 'Hanteltraining mit x Gewicht', 'te_25', 'te_44', 'sg_167');
+-- INSERT INTO trainingseinheiten VALUES ('TE_15', 'SZ-Bizeps-Curls', 10, 'Hanteltraining mit x Gewicht', 'te_07', 'te_87', 'sg_196');
+-- INSERT INTO trainingseinheiten VALUES ('TE_16', 'Butterfly', 10, 'Training an Maschine mit x Gewicht', 'te_01', 'te_33', 'sg_266');
+-- INSERT INTO trainingseinheiten VALUES ('TE_17', 'Reverse-Butterfly', 10, 'Training an Maschine mit x Gewicht', 'te_88', 'te_57', 'sg_874');
+-- INSERT INTO trainingseinheiten VALUES ('TE_18', 'Brustpresse', 15, 'Training an Maschine mit x Gewicht', 'te_72', 'te_95', 'sg_681');
+-- INSERT INTO trainingseinheiten VALUES ('TE_19', 'Beipresse', 12, 'Training an Maschine mit x Gewicht', 'te_77', 'te_08', 'sg_898');
+-- INSERT INTO trainingseinheiten VALUES ('TE_20', 'Bauchpresse', 10, 'Training an Maschine mit x Gewicht', 'te_66', 'te_75', 'sg_700');
+-- INSERT INTO trainingseinheiten VALUES ('TE_21', 'Rückenstrecker',10, 'Training an Maschine mit x Gewicht', 'te_35', 'te_98', 'sg_062');
+-- INSERT INTO trainingseinheiten VALUES ('TE_22', 'Beinstrecker',10, 'Training an Maschine mit x Gewicht', 'te_24', 'te_27', 'sg_071');
+-- INSERT INTO trainingseinheiten VALUES ('TE_23', 'Beinbeuger', 10, 'Training an Maschine mit x Gewicht', 'te_97', 'te_44', 'sg_385');
+-- INSERT INTO trainingseinheiten VALUES ('TE_24', 'Kniebeuge', 15, 'Hanteltraining mit x Gewicht', 'te_79', 'te_24', 'sg_129');
+-- INSERT INTO trainingseinheiten VALUES ('TE_25', 'Kreuzheben',20, 'Hanteltraining mit x Gewicht', 'te_02', 'te_20', 'sg_664');
+-- INSERT INTO trainingseinheiten VALUES ('TE_26', 'Schulterpresse',15, 'Hanteltraining mit x Gewicht', 'te_38', 'te_14', 'sg_448');
+-- INSERT INTO trainingseinheiten VALUES ('TE_27', 'Bein Abduktion',10, 'Training an Maschine mit x Gewicht', 'te_59', 'te_38', 'sg_032');
+-- INSERT INTO trainingseinheiten VALUES ('TE_28', 'Bein Adduktion',10, 'Training an Maschine mit x Gewicht', 'te_26', 'te_74', 'sg_001');
+-- INSERT INTO trainingseinheiten VALUES ('TE_29', 'Latisimuszug',12, 'Training am Kabelturm mit x Gewicht', 'te_99', 'te_94', 'sg_247');
+-- INSERT INTO trainingseinheiten VALUES ('TE_30', 'Rudern',12, 'Training am Kabelturm mit x Gewicht', 'te_35', 'te_97', 'sg_708');
+-- INSERT INTO trainingseinheiten VALUES ('TE_31', 'Trizeps Extension am Kabelturm',8, 'Training am Kabelturm mit x Gewicht', 'te_23', 'te_66', 'sg_152');
+-- INSERT INTO trainingseinheiten VALUES ('TE_32', 'Bizeps Curls am Kabelturm',8, 'Training am Kabelturm mit x Gewicht', 'te_75', 'te_41', 'sg_608');
+-- INSERT INTO trainingseinheiten VALUES ('TE_33', 'Klimmzüge',10, 'Körpergewichtsübung', 'te_61', 'te_96', 'sg_824');
+-- INSERT INTO trainingseinheiten VALUES ('TE_34', 'Dips',10, 'Körpergewichtsübung', 'te_61', 'te_96', 'sg_824');
 
 ------------------------------------------------------------------------------------------------------------------------
 -- Populate table trainingsplaene
 ------------------------------------------------------------------------------------------------------------------------
 
-INSERT INTO trainingsplaene VALUES ('TP_01', 'Beginner Friendly', 1, 6);
-INSERT INTO trainingsplaene VALUES ('TP_02', 'Ganzkörper', 2, 12);
-INSERT INTO trainingsplaene VALUES ('TP_03', 'Muskelaufbau', 4, 8);
-INSERT INTO trainingsplaene VALUES ('TP_04', '12-Wochen Challenge', 5, 12);
-INSERT INTO trainingsplaene VALUES ('TP_05', 'Zirkeltraining', 5, 12);
+INSERT INTO trainingsplaene VALUES ('TP_01', 'Beginner Friendly', 1, 2, 6);
+INSERT INTO trainingsplaene VALUES ('TP_02', 'Ganzkörper', 2, 3, 12);
+INSERT INTO trainingsplaene VALUES ('TP_03', 'Muskelaufbau', 4, 6, 8);
+INSERT INTO trainingsplaene VALUES ('TP_04', '12-Wochen Challenge', 5, 4, 12);
+INSERT INTO trainingsplaene VALUES ('TP_05', 'Zirkeltraining', 5, 5, 12);
 
 ------------------------------------------------------------------------------------------------------------------------
 -- Populate table durchfuehrungen
