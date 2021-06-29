823ccd3b0beae915cb5660aeef20dae120478e41
Okan Kaya <kaya.okan@live.de>
2021-06-29 19:14:29 +0200

    testdaten

---
+++ trainingseinheiten.sql
@@ -0,0 +1,59 @@
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_00', 'Stepper', 20, 'Stepper Cardio Training', null, 'TE_12', 'SG_001');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_01', 'Stepper', 20, 'Stepper Cardio Training', null, 'TE_16', 'SG_002');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_02', 'Stepper', 20, 'Stepper Cardio Training', null, 'TE_20', 'SG_003');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_03', 'Laufband', 20, 'Laufband Cardio Training', null, 'TE_13', 'SG_004');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_04', 'Laufband', 20, 'Laufband Cardio Training', null, 'TE_17', 'SG_005');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_05', 'Laufband', 20, 'Laufband Cardio Training', null, 'TE_21', 'SG_006');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_06', 'Crosstrainer', 30, 'Crosstrainer Workout', null, 'TE_14', 'SG_007');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_07', 'Crosstrainer', 30, 'Crosstrainer Workout', null, 'TE_18', 'SG_008');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_08', 'Crosstrainer', 30, 'Crosstrainer Workout', null, 'TE_22', 'SG_009');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_09', 'Biking', 30, 'Fahrrad fahren', null, 'TE_15', 'SG_010');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_10', 'Biking', 30, 'Fahrrad fahren', null, 'TE_19', 'SG_011');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_11', 'Biking', 30, 'Fahrrad fahren', null, 'TE_23', 'SG_012');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_12', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_00', null, 'SG_013');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_13', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_03', null, 'SG_013');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_14', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_06', null, 'SG_014');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_15', 'Bankdruecken', 15, 'Hanteltraining mit x Gewicht', 'TE_09', null, 'SG_014');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_13', 'Kazakh', 8, 'Seamless content-based pricing structure', 'te_57', 'te_37', 'sg_786');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_14', 'New Zealand Sign Language', 18, 'Exclusive homogeneous conglomeration', 'te_25', 'te_44', 'sg_167');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_15', 'Lao', 13, 'Centralized methodical time-frame', 'te_07', 'te_87', 'sg_196');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_16', 'French', 10, 'Ameliorated client-server array', 'te_01', 'te_33', 'sg_266');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_17', 'Norwegian', 19, 'Visionary human-resource open system', 'te_88', 'te_57', 'sg_874');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_18', 'Kashmiri', 9, 'Vision-oriented foreground instruction set', 'te_72', 'te_95', 'sg_681');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_19', 'Northern Sotho', 24, 'Centralized systemic conglomeration', 'te_77', 'te_08', 'sg_898');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_20', 'Dzongkha', 27, 'Assimilated foreground initiative', 'te_66', 'te_75', 'sg_700');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_21', 'Swedish', 6, 'Team-oriented systemic product', 'te_35', 'te_98', 'sg_062');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_22', 'Tok Pisin', 6, 'Upgradable grid-enabled instruction set', 'te_24', 'te_27', 'sg_071');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_23', 'Thai', 20, 'Organic tangible infrastructure', 'te_97', 'te_44', 'sg_385');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_24', 'Tsonga', 30, 'User-friendly bottom-line database', 'te_79', 'te_24', 'sg_129');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_25', 'Catalan', 9, 'Secured upward-trending attitude', 'te_02', 'te_20', 'sg_664');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_26', 'Bislama', 6, 'Customizable 5th generation analyzer', 'te_38', 'te_14', 'sg_448');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_27', 'Japanese', 29, 'Diverse systematic toolset', 'te_59', 'te_38', 'sg_032');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_28', 'Amharic', 18, 'Focused needs-based project', 'te_26', 'te_74', 'sg_001');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_29', 'Swahili', 30, 'Extended needs-based open architecture', 'te_99', 'te_94', 'sg_247');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_30', 'Japanese', 17, 'Multi-layered global intranet', 'te_35', 'te_97', 'sg_708');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_31', 'New Zealand Sign Language', 20, 'Multi-tiered background alliance', 'te_23', 'te_66', 'sg_152');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_32', 'Burmese', 26, 'Multi-layered empowering toolset', 'te_75', 'te_41', 'sg_608');
+INSERT INTO TRAININGSEINHEITEN VALUES ('TE_33', 'German', 18, 'Programmable hybrid service-desk', 'te_61', 'te_96', 'sg_824');
+
+3x stepper
+3x laufband
+3x fahrrad
+3x crosstrainer
+2x Hantelbank
+2x Schraegbank
+2x SZ-Stange
+1x Brustpresse
+1x Beinpresse
+3x Fitnessmatte
+1x Schulterpresse
+1x Bauchpresse
+2x Kabelturm
+2x Rückenstrecker
+1x Beinbeuger
+1x Beinstrecker
+1x Butterfly
+1x Abduktionsmaschine
+1x Adduktionsmaschine
+1x Latismuszug
+1x Rudermaschine
