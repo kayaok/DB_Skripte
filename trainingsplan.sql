603d2c00cbad3ee82bde2b00267018505924832f
Okan Kaya <kaya.okan@live.de>
2021-06-29 19:39:37 +0200

    durchfuehrung und trainingsplan

--- trainingsplan.sql
+++ trainingsplan.sql
@@ -1,9 +1,5 @@
-insert into TRAININGSPLAENE values (TP_001, 'Beginner Friendly', 1, 6);
-insert into TRAININGSPLAENE values (TP_002, 'Ganzkörper', 2, 12);
-insert into TRAININGSPLAENE values (TP_003, 'Muskelaufbau', 4, 8);
-insert into TRAININGSPLAENE values (TP_004, '12-Wochen Challenge', 5, 12);
-insert into TRAININGSPLAENE values (TP_005, 'Zirkeltraining', 5, 12);
-insert into TRAININGSPLAENE values (TP_006, 'IronGym', 3, 16);
-insert into TRAININGSPLAENE values (TP_007, '6-Wochen-Diät', 3, 6);
-insert into TRAININGSPLAENE values (TP_008, '30-Tage-Bein Challenge', 2, 4);
-insert into TRAININGSPLAENE values (TP_009, 'Flat ABS Challenge', 2, 4);
+insert into TRAININGSPLAENE values (TP_01, 'Beginner Friendly', 1, 6);
+insert into TRAININGSPLAENE values (TP_02, 'Ganzkörper', 2, 12);
+insert into TRAININGSPLAENE values (TP_03, 'Muskelaufbau', 4, 8);
+insert into TRAININGSPLAENE values (TP_04, '12-Wochen Challenge', 5, 12);
+insert into TRAININGSPLAENE values (TP_05, 'Zirkeltraining', 5, 12);
