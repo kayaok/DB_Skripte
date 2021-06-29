09f9c3360fd322e606c7f02797afb178fd87b26d
Till-G <t.greiffertmail.com>
2021-06-30 01:01:41 +0200

    was macht den DATE da?

--- tables.sql
+++ tables.sql
@@ -439,7 +439,7 @@ CREATE TABLE sportgeraete
         CONSTRAINT sportgeraet_ad_nn NOT NULL,
     garantie           DATE
         CONSTRAINT sportgeraet_garantie_nn NOT NULL,
-    anschaffungskosten DATE
+    anschaffungskosten DECIMAL(4, 2)
         CONSTRAINT sportgeraet_ak_nn NOT NULL,
     e_gym              CHAR(1)
         CONSTRAINT sportgeraet_eg_nn NOT NULL
