c1aeab83a075842889169f1f4c269afcf50f3cf0
iremB <irem.belik@student.reutlingen.university.de>
2021-06-29 16:51:38 +0200

    Testdaten

--- trainer.sql
+++ trainer.sql
@@ -1,6 +1,4 @@
 insert into TRAINER values ('A','M_05');
 insert into TRAINER values ('C','M_06');
 insert into TRAINER values ('B','M_08');
-insert into TRAINER values ('C','M_13');
-insert into TRAINER values ('B','M_14');
 commit;
