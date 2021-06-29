f5144300784af8f8cb53461aa830a3e98f599db8
iremB <irem.belik@student.reutlingen.university.de>
2021-06-29 21:12:06 +0200

    KUNDEEEN

---
+++ leitet.sql
@@ -0,0 +1,8 @@
+insert into leitet values ('AV_05');
+insert into leitet values ('AV_06');
+insert into leitet values ('AV_08');
+insert into leitet values ();
+insert into leitet values ();
+insert into leitet values ();
+insert into leitet values ();
+insert into leitet values ();
