bcadf9353db9352955d13b4b304cd86f89deb414
Till-G <t.greiffertmail.com>
2021-06-30 02:15:22 +0200

    ich mach hier nur sauber...

---
+++ arbeitsvertraege.sql
@@ -0,0 +1,10 @@
+INSERT INTO arbeitsvertraege VALUES ('AV_01', '450,00', 'Aushilfe', 20);
+INSERT INTO arbeitsvertraege VALUES ('AV_02', '3000,00', 'Manager',40);
+INSERT INTO arbeitsvertraege VALUES ('AV_03', '850,00', 'Ausbildung', 36);
+INSERT INTO arbeitsvertraege VALUES ('AV_04', '450', 'Aushilfe', 20);
+INSERT INTO arbeitsvertraege VALUES ('AV_05', '2500,00', 'Trainer', 40);
+INSERT INTO arbeitsvertraege VALUES ('AV_06', '2300,00', 'Trainer', 38);
+INSERT INTO arbeitsvertraege VALUES ('AV_07', '450,00', 'Aushilfe', 20);
+INSERT INTO arbeitsvertraege VALUES ('AV_08', '2500,00', 'Trainer', 40);
+INSERT INTO arbeitsvertraege VALUES ('AV_09', '900,00', 'Ausbildung', 37);
+
