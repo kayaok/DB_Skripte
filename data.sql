74ee1f6a399778efaa4b0bbb631915cd69b0f121
Till-G <t.greiffertmail.com>
2021-06-30 15:16:40 +0200

    Die Früchte sind vielsamige, kahle Beeren, deren Größe stark zwischen 50 mal 50 und 300 mal 150 mm variiert, auch die Form kann sehr unterschiedlich ausfallen, beispielsweise länglich, eiförmig, rund oder länglich-birnenförmig. Die Farbe des Exokarps ist schwarz-violett, dunkelviolett, gelblich oder weißlich.[5][6] Die flachen Samen sind mehr oder weniger nierenförmig bis rundlich, orange-ockerfarben, etwa 2,8–3,5 mm groß und feingrubig.

--- data.sql
+++ data.sql
@@ -1173,6 +1173,30 @@ INSERT INTO ist_teilnehmer VALUES ('KRS_06', 'C_026');
 COMMIT;
 
 ------------------------------------------------------------------------------------------------------------------------
+-- Populate table besteht_aus
+------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO besteht_aus VALUES ('KRS_00', 'TE_42');
+INSERT INTO besteht_aus VALUES ('KRS_00', 'TE_43');
+INSERT INTO besteht_aus VALUES ('KRS_00', 'TE_45');
+INSERT INTO besteht_aus VALUES ('KRS_01', 'TE_46');
+INSERT INTO besteht_aus VALUES ('KRS_01', 'TE_42');
+INSERT INTO besteht_aus VALUES ('KRS_01', 'TE_45');
+INSERT INTO besteht_aus VALUES ('KRS_02', 'TE_41');
+INSERT INTO besteht_aus VALUES ('KRS_02', 'TE_45');
+INSERT INTO besteht_aus VALUES ('KRS_02', 'TE_42');
+INSERT INTO besteht_aus VALUES ('KRS_03', 'TE_42');
+INSERT INTO besteht_aus VALUES ('KRS_03', 'TE_45');
+INSERT INTO besteht_aus VALUES ('KRS_03', 'TE_43');
+INSERT INTO besteht_aus VALUES ('KRS_04', 'TE_42');
+INSERT INTO besteht_aus VALUES ('KRS_05', 'TE_44');
+INSERT INTO besteht_aus VALUES ('KRS_05', 'TE_45');
+INSERT INTO besteht_aus VALUES ('KRS_06', 'TE_42');
+INSERT INTO besteht_aus VALUES ('KRS_06', 'TE_43');
+
+COMMIT;
+
+------------------------------------------------------------------------------------------------------------------------
 -- Populate table kundenvertraege
 ------------------------------------------------------------------------------------------------------------------------
 
